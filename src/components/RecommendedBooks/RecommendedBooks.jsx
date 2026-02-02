b8572c4903e45af7b18b1fdfbc648ef51e8fe8a8
Natali258 <natalya.holyavko@gmail.com>
2026-02-02 17:45:51 +0200

    add booksList

--- src/components/RecommendedBooks/RecommendedBooks.jsx
+++ src/components/RecommendedBooks/RecommendedBooks.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { InitialState } from '../api/booksAPI'
 
 export const RecommendedBooks = () => {
+    const books = InitialState.books;
+    console.log(books);
+    
   return (
     <div>
         <h1>Recommended</h1>
@@ -8,7 +12,11 @@ export const RecommendedBooks = () => {
             <button>Left</button>
             <button>Right</button>
         </div>
-        <ul></ul>
+        <ul>{books.map((book) => <li key={book._id}>
+            <img src={book.imageUrl} alt={book.title} width="100"/>
+            <h2>{book.title}</h2>
+            <p>{book.author}</p>
+        </li>)}</ul>
     </div>
   )
 }
