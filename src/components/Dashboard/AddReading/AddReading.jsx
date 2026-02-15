6c9aeed343d89edd8e55c92b4d81c08c07d05edd
Natali258 <natalya.holyavko@gmail.com>
2026-02-15 22:50:04 +0200

    add styled AddReading

--- src/components/Dashboard/AddReading/AddReading.jsx
+++ src/components/Dashboard/AddReading/AddReading.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { AddReadingBtn, AddReadingForm, AddReadingInput, AddReadingTitle } from './AddReading.styled'
 
 export const AddReading = () => {
   return (
     <div>
-      <p>Start page:</p>
-      <form action="">
-        <input type="text" />
-        <button type='submit'>To start</button>
-      </form>
+      <AddReadingTitle>Start page:</AddReadingTitle>
+      <AddReadingForm action="">
+        <AddReadingInput type="text" placeholder='Page number:'  />
+        <AddReadingBtn type='submit'>To start</AddReadingBtn>
+      </AddReadingForm>
     </div>
   )
 }
--- src/components/Dashboard/AddReading/AddReading.styled.js
+++ src/components/Dashboard/AddReading/AddReading.styled.js
@@ -2,3 +2,51 @@ import s from 'styled-components'
 
 export const AddReadingContainer = s.div`
 display: block;`
+
+export const AddReadingTitle = s.p`
+font-family: var(--font-family);
+font-weight: 500;
+font-size: 14px;
+line-height: 129%;
+letter-spacing: -0.02em;
+color: var(--main-text-color);`
+
+export const AddReadingForm = s.form`
+display: flex;
+flex-direction: column;
+gap: 20px;`
+
+export const AddReadingInput = s.input`
+box-sizing: border-box;
+padding: 16px 14px;
+background-color: var(--select-background-color);
+border-radius: 12px;
+border: none;
+
+&:hover, &:focus-visible {
+    outline: none;
+    border: 1px solid var(--border-color);
+}`
+
+export const AddReadingBtn = s.button`
+padding: 12px 28px;
+align-self: flex-start;
+background-color: var(--container-background-color);
+border-radius: 30px;
+border: 1px solid var(--border-color);
+margin-top: 12px;
+
+font-family: Gilroy;
+font-weight: 700;
+font-style: Bold;
+font-size: 16px;
+leading-trim: NONE;
+line-height: 18px;
+letter-spacing: 2%;
+color: var(--main-text-color);
+
+&:hover {
+    background-color: var(--main-text-color);
+    color: var(--body-background-color);
+}
+`
