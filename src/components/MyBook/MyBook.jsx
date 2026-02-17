8aa048d2b47fb0bd32cdadea8c1a17707083d27b
Natali258 <natalya.holyavko@gmail.com>
2026-02-17 17:30:23 +0200

    add IconButton for MyBook

--- src/components/MyBook/MyBook.jsx
+++ src/components/MyBook/MyBook.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { MyBookAuthor, MyBookBox, MyBookContainer, MyBookImg, MyBookName, MyBookTitle } from './MyBook.styled'
+import { IconButton, Inner, MyBookAuthor, MyBookBox, MyBookContainer, MyBookName, MyBookTitle } from './MyBook.styled'
 
 export const MyBook = () => {
   return (
     <MyBookContainer>
       <MyBookTitle>My reading</MyBookTitle>
       <MyBookBox>
-        <MyBookImg src="/src/assets/img/books-title.png" alt="books-title" />
+        <img src="/src/assets/img/books-title.png" alt="books-title" />
         <MyBookName>I See You Are Interested In The Dark</MyBookName>
         <MyBookAuthor>Hilarion Pavlyuk</MyBookAuthor>
-        <button>Кнопка</button>
+        <IconButton><Inner /></IconButton>
       </MyBookBox>
     </MyBookContainer>
   )
--- src/components/MyBook/MyBook.styled.js
+++ src/components/MyBook/MyBook.styled.js
@@ -1,7 +1,6 @@
 import s from 'styled-components';
 
 export const MyBookContainer = s.div`
-position: relative;
 display: flex;
 flex-direction: column;
 gap: 44px;
@@ -10,13 +9,6 @@ border-radius: 30px;
 background-color: var(--container-background-color);
 min-width: 847px;
 `
-
-export const MyBookBox = s.div`
-text-align: center;
-`
-export const MyBookImg = s.img`
-`
-
 export const MyBookTitle = s.h2`
 font-family: var(--font-family);
 font-weight: 700;
@@ -24,6 +16,10 @@ font-size: 28px;
 line-height: 114%;
 letter-spacing: 0.02em;
 color: var(--main-text-color);`
+
+export const MyBookBox = s.div`
+text-align: center;
+`
 
 export const MyBookName = s.p`
 margin-top: 25px;
@@ -43,3 +39,39 @@ font-size: 14px;
 line-height: 129%;
 letter-spacing: -0.02em;
 color: var(--grey-text-color);`
+
+// Button
+
+export const IconButton = s.button`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 2px solid #fff;
+  background: #0f0f0f;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  padding: 0;
+  transition: transform 200ms ease;
+
+  &:hover {
+    transform: scale(1.08);
+  }
+`;
+
+export const Inner = s.div`
+  width: 40px;
+  height: 40px;
+  background: #ff0000;
+  border-radius: 50%;
+  transition: transform 200ms ease;
+
+  ${IconButton}:focus & {
+    width: 18px;
+    height: 18px;
+    background: #ff0000;
+    border-radius: 4px;
+    transform: scale(1.15) rotate(90deg);
+  }
+`;
