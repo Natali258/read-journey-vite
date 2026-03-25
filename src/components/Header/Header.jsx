4fdef03970755d3172d167c4e347fcf6df8850ec
Natali258 <natalya.holyavko@gmail.com>
2026-03-25 13:32:55 +0200

    fix UserBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,17 @@ import { NavMenu } from '../NavMenu/NavMenu'
 import Logo from '../Logo/Logo'
 import { UserBar } from '../UserBar/UserBar'
 import { StlBtnLogOut, StlHeaderContainer } from './Header.styled'
-import { useDispatch } from 'react-redux'
+import { useDispatch} from 'react-redux'
 import { logoutThunk } from '../../redux/authSlice/operations'
+
 
 export const Header = () => {
   const dispatch = useDispatch()
   const logOut = () => {
       dispatch(logoutThunk())
   }
+  
+  
   return (
     <StlHeaderContainer>
       <Logo/>
--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { StlUserContainer, StlUserName, StlUserPhoto } from './UserBar.styled'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../redux/authSlice/AuthSlice';
 
 export const UserBar = () => {
+
+  const user = useSelector(selectUser);
+  const firstLetter = user.name[0].toUpperCase();
   return (
     <StlUserContainer>
         <StlUserPhoto>
-            I
+            {firstLetter}
         </StlUserPhoto>
-        <StlUserName>Ilona Ratushniak</StlUserName>
+        <StlUserName>{user.name}</StlUserName>
     </StlUserContainer>
   )
 }
