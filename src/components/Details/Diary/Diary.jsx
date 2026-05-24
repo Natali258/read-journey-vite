6801e3f8c7657abe6149dde4d7a143cdbf13bd74
Natali258 <natalya.holyavko@gmail.com>
2026-05-24 22:17:33 +0300

    add Diary.styled

--- src/components/Details/Diary/Diary.jsx
+++ src/components/Details/Diary/Diary.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import Icon from "../../Icon/Icon";
+import { DiaryHeaderContainer, DiaryIconContainer, DiaryTitle, DiaryUl } from "./Diary.styled";
 
 export const Diary = () => {
   return (
     <div>
-      <p>Diary</p>
-      <div>
-        <div>
+      <DiaryHeaderContainer>
+        <DiaryTitle>Diary</DiaryTitle>
+        <DiaryIconContainer>
           <Icon
-            name="icon-hourglass"
-            size={{ width: 20, height: 20 }}
-            color="var(--main-text-color)"
-          />
-        </div>
-        <div>
+              name="icon-hourglass"
+              size={{ width: 20, height: 20 }}
+              color="var(--main-text-color)"
+            />
           <Icon
-            name="icon-pie-chart"
-            size={{ width: 20, height: 20 }}
-            color="var(--icon-diary-color)"
-            secondaryColor="var(--grey-text-color)"
-          />
-        </div>
-      </div>
-      {/* <ul>
-        <DiaryItem />
-      </ul> */}
+              name="icon-pie-chart"
+              size={{ width: 20, height: 20 }}
+              color="var(--icon-diary-color)"
+              secondaryColor="var(--grey-text-color)"
+            />
+        </DiaryIconContainer>
+      </DiaryHeaderContainer>
+      <DiaryUl>11111
+        {/* <DiaryItem /> */}
+      </DiaryUl>
     </div>
   );
 };
---
+++ src/components/Details/Diary/Diary.styled.js
@@ -0,0 +1,29 @@
+import s from 'styled-components'
+
+export const DiaryHeaderContainer = s.div`
+  display: flex;
+  margin-top: 20px;
+`
+export const DiaryTitle = s.p`
+display: inline-block;
+
+font-family: var(--font-family);
+font-weight: 700;
+font-size: 20px;
+line-height: 100%;
+letter-spacing: -0.02em;
+color: var(--main-text-color);
+`
+export const DiaryIconContainer = s.div`
+  display: inline-flex;
+  margin-left:auto;
+  gap: 8px;
+`
+export const DiaryUl = s.ul`
+  display: flex;
+  gap: 22px;
+  border-radius: 12px;
+  background-color: var(--select-background-color);
+  padding: 20px;
+  margin-top: 20px;
+`
