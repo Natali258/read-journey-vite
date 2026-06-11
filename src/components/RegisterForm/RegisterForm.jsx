85e4800191ffa2d90337b0a4b8342a5f0a6f9312
Natali258 <natalya.holyavko@gmail.com>
2026-06-11 12:52:40 +0300

    add helper/media

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,32 +1,54 @@
-import React from 'react'
-import Logo from '../Logo/Logo'
-import { StlBtnRegister, StlFieldsRegister, StlFormRegister, StlInputRegister, StlNavLinkLogin, StlRegisterContainer, StlTitleRegister } from './RegisterForm.styled'
-import { useDispatch } from 'react-redux';
-import { registerThunk } from '../../redux/authSlice/operations';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import Logo from "../Logo/Logo";
+import {
+  StlBtnRegister,
+  StlFieldsRegister,
+  StlFormRegister,
+  StlInputRegister,
+  StlNavLinkLogin,
+  StlRegisterContainer,
+  StlTitleRegister,
+} from "./RegisterForm.styled";
+import { useDispatch } from "react-redux";
+import { registerThunk } from "../../redux/authSlice/operations";
+import { useForm } from "react-hook-form";
 
 const RegisterForm = () => {
-  const {register, handleSubmit, reset} = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const dispatch = useDispatch();
-  const submit = data => {
+  const submit = (data) => {
     dispatch(registerThunk(data));
     reset();
   };
-  
+
   return (
     <StlRegisterContainer>
-        <Logo/>
-        <StlTitleRegister>Expand your mind, reading <span>a book</span></StlTitleRegister>
-        <StlFormRegister onSubmit={handleSubmit(submit)}>
-            <StlFieldsRegister>
-                <StlInputRegister type="text" placeholder='Name:' {...register('name')} />
-                <StlInputRegister type="text" placeholder='Mail:'{...register('email')}/>
-                <StlInputRegister type="text" placeholder='Password:' {...register('password')}/>
-            </StlFieldsRegister>
-            <StlBtnRegister type='submit'>Registration</StlBtnRegister>
-        </StlFormRegister>
-        <StlNavLinkLogin to='/login'>Already have an account?</StlNavLinkLogin>
+      <Logo />
+      <StlTitleRegister>
+        Expand your mind, reading <span>a book</span>
+      </StlTitleRegister>
+      <StlFormRegister onSubmit={handleSubmit(submit)}>
+        <StlFieldsRegister>
+          <StlInputRegister
+            type="text"
+            placeholder="Name:"
+            {...register("name")}
+          />
+          <StlInputRegister
+            type="text"
+            placeholder="Mail:"
+            {...register("email")}
+          />
+          <StlInputRegister
+            type="text"
+            placeholder="Password:"
+            {...register("password")}
+          />
+        </StlFieldsRegister>
+        <StlBtnRegister type="submit">Registration</StlBtnRegister>
+      </StlFormRegister>
+      <StlNavLinkLogin to="/login">Already have an account?</StlNavLinkLogin>
     </StlRegisterContainer>
-  )
-}
-export default RegisterForm
+  );
+};
+export default RegisterForm;
---
+++ src/helper/media.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const media = {
+    mobileSmall: "@media (min-width: 320px)",
+    mobile: "@media (min-width: 375px)",
+    tablet: "@media (min-width: 768px)",
+    desktop: "@media (min-width: 1440px)"
+}
