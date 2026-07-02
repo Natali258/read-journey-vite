a83d7342b7cfbe445729cb9767f81e37966dbafa
Natali258 <natalya.holyavko@gmail.com>
2026-07-02 16:44:00 +0300

    add base: '/read-journey-vite/',

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/read-journey-vite/login',
+  base: '/read-journey-vite/',
   plugins: [
     react({
       babel: {
