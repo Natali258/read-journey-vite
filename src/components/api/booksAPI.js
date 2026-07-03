d64be2ad184fece17741ab0a6f03cb1fd0648ff0
Natali258 <natalya.holyavko@gmail.com>
2026-07-03 18:01:54 +0300

    coment bookApi

--- src/components/api/booksAPI.js
+++ src/components/api/booksAPI.js
@@ -1,100 +1,100 @@
-export const InitialState ={
-  books: [
-    {
-      "_id": "654fc4d00a563c69b09895ef",
-      "title": "Lovers of Justice",
-      "author": "Yuri Andrukhovych",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
-      "totalPages": 304,
-      "recommend": true
-    },
-    {
-      "_id": "654fc5064f56fe7a8d0e19eb",
-      "title": "It doesn't hurt",
-      "author": "Kateryna Babkina",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
-      "totalPages": 72,
-      "recommend": true
-    },
-    {
-      "_id": "654fc4d00a563c69b09895ef",
-      "title": "Lovers of Justice",
-      "author": "Yuri Andrukhovych",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
-      "totalPages": 304,
-      "recommend": true
-    },
-    {
-      "_id": "654fc5064f56fe7a8d0e19eb",
-      "title": "It doesn't hurt",
-      "author": "Kateryna Babkina",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
-      "totalPages": 72,
-      "recommend": true
-    },
-    {
-      "_id": "654fc4d00a563c69b09895ef",
-      "title": "Lovers of Justice",
-      "author": "Yuri Andrukhovych",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
-      "totalPages": 304,
-      "recommend": true
-    },
-    {
-      "_id": "654fc5064f56fe7a8d0e19eb",
-      "title": "It doesn't hurt",
-      "author": "Kateryna Babkina",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
-      "totalPages": 72,
-      "recommend": true
-    },
-    {
-      "_id": "654fc4d00a563c69b09895ef",
-      "title": "Lovers of Justice",
-      "author": "Yuri Andrukhovych",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
-      "totalPages": 304,
-      "recommend": true
-    },
-    {
-      "_id": "654fc5064f56fe7a8d0e19eb",
-      "title": "It doesn't hurt",
-      "author": "Kateryna Babkina",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
-      "totalPages": 72,
-      "recommend": true
-    },
-    {
-      "_id": "654fc4d00a563c69b09895ef",
-      "title": "Lovers of Justice",
-      "author": "Yuri Andrukhovych",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
-      "totalPages": 304,
-      "recommend": true
-    },
-    {
-      "_id": "654fc5064f56fe7a8d0e19eb",
-      "title": "It doesn't hurt",
-      "author": "Kateryna Babkina",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
-      "totalPages": 72,
-      "recommend": true
-    },
-    {
-      "_id": "654fc4d00a563c69b09895ef",
-      "title": "Lovers of Justice",
-      "author": "Yuri Andrukhovych",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
-      "totalPages": 304,
-      "recommend": true
-    },
-    {
-      "_id": "654fc5064f56fe7a8d0e19eb",
-      "title": "It doesn't hurt",
-      "author": "Kateryna Babkina",
-      "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
-      "totalPages": 72,
-      "recommend": true
-    }
-  ]
-}
+// export const InitialState ={
+//   books: [
+//     {
+//       "_id": "654fc4d00a563c69b09895ef",
+//       "title": "Lovers of Justice",
+//       "author": "Yuri Andrukhovych",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
+//       "totalPages": 304,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc5064f56fe7a8d0e19eb",
+//       "title": "It doesn't hurt",
+//       "author": "Kateryna Babkina",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
+//       "totalPages": 72,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc4d00a563c69b09895ef",
+//       "title": "Lovers of Justice",
+//       "author": "Yuri Andrukhovych",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
+//       "totalPages": 304,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc5064f56fe7a8d0e19eb",
+//       "title": "It doesn't hurt",
+//       "author": "Kateryna Babkina",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
+//       "totalPages": 72,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc4d00a563c69b09895ef",
+//       "title": "Lovers of Justice",
+//       "author": "Yuri Andrukhovych",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
+//       "totalPages": 304,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc5064f56fe7a8d0e19eb",
+//       "title": "It doesn't hurt",
+//       "author": "Kateryna Babkina",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
+//       "totalPages": 72,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc4d00a563c69b09895ef",
+//       "title": "Lovers of Justice",
+//       "author": "Yuri Andrukhovych",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
+//       "totalPages": 304,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc5064f56fe7a8d0e19eb",
+//       "title": "It doesn't hurt",
+//       "author": "Kateryna Babkina",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
+//       "totalPages": 72,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc4d00a563c69b09895ef",
+//       "title": "Lovers of Justice",
+//       "author": "Yuri Andrukhovych",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
+//       "totalPages": 304,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc5064f56fe7a8d0e19eb",
+//       "title": "It doesn't hurt",
+//       "author": "Kateryna Babkina",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
+//       "totalPages": 72,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc4d00a563c69b09895ef",
+//       "title": "Lovers of Justice",
+//       "author": "Yuri Andrukhovych",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
+//       "totalPages": 304,
+//       "recommend": true
+//     },
+//     {
+//       "_id": "654fc5064f56fe7a8d0e19eb",
+//       "title": "It doesn't hurt",
+//       "author": "Kateryna Babkina",
+//       "imageUrl": "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
+//       "totalPages": 72,
+//       "recommend": true
+//     }
+//   ]
+// }
